11415625affe690bc741d30d82d15f122d88d6c2
vknanavati <vimin.nanavati@gmail.com>
2025-01-24 21:06:07 -0500

    created Wallpaper file.

---
+++ src/components/Wallpaper.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ReactNode } from 'react';
+import {Box} from '@mui/material';
+import BACKGROUND from '../images/background.png'
+
+export function Wallpaper({children}) {
+    return (
+        <Box
+        sx={{
+            backgroundImage: `url(${BACKGROUND})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+            backgroundAttachment: { xs: 'scroll', sm: 'fixed' }
+        }}
+        >
+            <Box
+                sx={{
+                    width: '100%',
+                    overflowX: 'hidden',
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+            >
+                {children}
+            </Box>
+        </Box>
+    )
+}
