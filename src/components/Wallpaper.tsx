7804f463b73f834b72304b7bf148b46a5e377ab2
vknanavati <vimin.nanavati@gmail.com>
2025-01-29 11:04:29 -0500

    adjusted indentation and changed single quotation marks wih double quotation marks.

--- src/components/Wallpaper.tsx
+++ src/components/Wallpaper.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactNode } from 'react';
-import {Box} from '@mui/material';
+import { Box } from '@mui/material';
 import BACKGROUND from '../images/background.png'
 
 interface WallpaperProps {
@@ -10,25 +10,25 @@ interface WallpaperProps {
 export function Wallpaper({children}: WallpaperProps) {
     return (
         <Box
-        sx={{
-            backgroundImage: `url(${BACKGROUND})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-            minHeight: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            overflow: 'hidden',
-            backgroundAttachment: { xs: 'scroll', sm: 'fixed' }
-        }}
+            sx={{
+                backgroundImage: `url(${BACKGROUND})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                width: "100vw",
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                overflow: "hidden",
+                backgroundAttachment: { xs: "scroll", sm: "fixed" }
+            }}
         >
             <Box
                 sx={{
-                    width: '100%',
-                    overflowX: 'hidden',
-                    display: 'flex',
-                    flexDirection: 'column'
+                    width: "100%",
+                    overflowX: "hidden",
+                    display: "flex",
+                    flexDirection: "column"
                 }}
             >
                 {children}
